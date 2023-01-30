5a686e7e7131c205668e6b3192371960b66bf42e
Sergey Kudashkin <ya.sikuda@yandex.ru>
2023-01-30 17:39:55 +0300

    first step of navigation

--- app/src/main/java/ru/sikuda/mobile/h_gr/MainActivity.kt
+++ app/src/main/java/ru/sikuda/mobile/h_gr/MainActivity.kt
@@ -1,21 +1,19 @@
 package ru.sikuda.mobile.h_gr
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,128 +22,122 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import kotlinx.coroutines.launch
 import ru.sikuda.mobile.h_gr.ui.tasks.TasksList
 import ru.sikuda.mobile.h_gr.ui.theme.H_grTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             H_grTheme {
+                val navController = rememberNavController()
+
                 val scaffoldState = rememberScaffoldState()
                 val scope = rememberCoroutineScope()
 
-                var winState by remember { mutableStateOf( AppDestination.HOME) }
+                //var winState by remember { mutableStateOf( AppDestination.HOME) }
                 val buttonsList = arrayOf("Задания", "Разгрузки", "Навалы")
 
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    topBar = {
-                        AppBar(
-                            onNavigationIconClick = {
-                                scope.launch {
-                                    scaffoldState.drawerState.open()
-                                }
-                            }
-                        )
-                    },
-                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
-                    drawerContent = {
-                        DrawerHeader()
-                        DrawerBody(
-                            items = listOf(
-                                MenuItem(
-                                    id = "home",
-                                    title = "Начало",
-                                    contentDescription = "Go to home screen",
-                                    icon = Icons.Default.Home,
-                                ),
-                                MenuItem(
-                                    id = "tasks",
-                                    title = "List of tasks",
-                                    contentDescription = "Go to list of tasks",
-                                    icon = Icons.Default.Settings
-                                ),
-                                MenuItem(
-                                    id = "help",
-                                    title = "Help",
-                                    contentDescription = "Get help",
-                                    icon = Icons.Default.Info
-                                ),
-                            ),
-                            onItemClick = {
-                                scope.launch {
-                                    scaffoldState.drawerState.close()
-                                    when(it.id){
-                                        "settings" -> winState = AppDestination.TASKS
-                                    }
-                                }
-                            }
-                        )
-                    }
-                ) {
-                    it
-                    when(winState){
-                        AppDestination.TASKS -> TasksList()
-                        else -> {
-                            Surface(
-                                modifier = Modifier.fillMaxSize(),
-                                color = MaterialTheme.colors.background,
-                            ) {
-
-                                LazyVerticalGrid(
-                                    GridCells.Fixed(2),
-                                    horizontalArrangement = Arrangement.Center,
-                                    verticalArrangement = Arrangement.Center
-
-                                ) {
-                                    items(buttonsList.size) { item ->
-
-                                        Column(
-                                            horizontalAlignment = Alignment.CenterHorizontally,
-                                            modifier = Modifier
-                                                .padding(20.dp)
-                                                .selectable(true, onClick = {
-                                                    when (buttonsList[item]){
-                                                        "Задания" -> winState = AppDestination.TASKS
-                                                    }
-                                                })
-                                        ) {
-
-                                            Image(
-                                                ImageVector.vectorResource(R.drawable.ic_launcher_background),
-                                                contentDescription = buttonsList[item],
-                                                alignment = Alignment.TopCenter
-                                            )
-
-                                            Text(
-                                                text = buttonsList[item],
-                                                fontSize = 16.sp,
-                                                textAlign = TextAlign.Center,
-                                            )
-                                        }
-                                    }
+                Scaffold(scaffoldState = scaffoldState, topBar = {
+                    AppBar(onNavigationIconClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    })
+                }, drawerGesturesEnabled = scaffoldState.drawerState.isOpen, drawerContent = {
+                    DrawerHeader()
+                    DrawerBody(items = listOf(
+                        MenuItem(
+                            id = "home",
+                            title = "Начало",
+                            contentDescription = "Go to home screen",
+                            icon = Icons.Default.Home,
+                        ),
+                        MenuItem(
+                            id = "tasks",
+                            title = "List of tasks",
+                            contentDescription = "Go to list of tasks",
+                            icon = Icons.Default.Settings
+                        ),
+                        MenuItem(
+                            id = "help",
+                            title = "Help",
+                            contentDescription = "Get help",
+                            icon = Icons.Default.Info
+                        ),
+                    ), onItemClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                            when (it.id) {
+                                "tasks" -> navController.navigate("${NavRoute.TASKS}/Common tasks")
+                                else -> navController.navigate(NavRoute.HOME) {
+                                    popUpTo(NavRoute.HOME) { inclusive = true }
                                 }
                             }
                         }
-                    }
+                    })
+                }) { paddingValues ->
+                    MyNavHost(navController, paddingValues)
                 }
             }
         }
     }
 }
 
-enum class AppDestination(name: String) {
 
-    HOME("Home"),
-    TASKS("Tasks"),
-    TASK_DETAIL_ROUTE("Task"),
-
+object NavRoute {
+    const val HOME = "Home"
+    const val TASKS = "Tasks"
+    const val TASK_DETAIL = "Task Detail"
 }
 
 @Composable
-fun MainView( ){
+fun MyNavHost(navHostController: NavHostController, paddingValues: PaddingValues) {
+    NavHost(
+        navController = navHostController, startDestination = NavRoute.HOME
+    ) {
+        val routeTasks = "${NavRoute.TASKS}/{argument}"
+        val routeTaskDetail = "${NavRoute.TASK_DETAIL}/{visible}"
+        composable(NavRoute.HOME) {
+            MainView {
+                navHostController.navigate("${NavRoute.TASKS}/Common tasks")
+            }
+        }
+        composable(
+            routeTasks, arguments = listOf(navArgument(name = "argument") {})
+        ) { navEntry ->
+            TasksList(navEntry.arguments?.getString("argument")) {
+                navHostController.navigate("${NavRoute.TASKS}/false")
+            }
+        }
+//        composable(
+//            routeWithArgumentsC,
+//            arguments = listOf(navArgument("visible"){
+//                type = NavType.BoolType
+//            }),
+//        ){
+//            ScreenC(it.arguments?.getBoolean("visible")){
+//                navHostController.navigate(NavRoute.HOME){
+//                    popUpTo(NavRoute.HOME){inclusive = true}
+//                }
+//            }
+//        }
+
+    }
+}
+
+@Composable
+fun MainView(
+    onNavigationTasks: () -> Unit
+) {
     val buttonsList = arrayOf("Задания", "Разгрузки", "Навалы")
 
     Surface(
@@ -166,7 +158,9 @@ fun MainView( ){
                     modifier = Modifier
                         .padding(20.dp)
                         .selectable(true, onClick = {
-
+                            when (item) {
+                                0 -> onNavigationTasks()
+                            }
                         })
                 ) {
 
--- app/src/main/java/ru/sikuda/mobile/h_gr/ui/tasks/TasksList.kt
+++ app/src/main/java/ru/sikuda/mobile/h_gr/ui/tasks/TasksList.kt
@@ -1,5 +1,8 @@
 package ru.sikuda.mobile.h_gr.ui.tasks
 
+import android.app.Application
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,9 +14,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 data class TaskItem(
@@ -22,24 +27,32 @@ data class TaskItem(
 )
 
 @Composable
-fun TasksList() {
+fun TasksList(name: String?, onNavigation: () -> Unit) {
 
-    val items = listOf(TaskItem(1, "Text1"), TaskItem(2, "Text2"))
+    val context = LocalContext.current
+
+    val items = listOf(
+        TaskItem( 1, "Text1"),
+        TaskItem( 2, "Text2"),
+        TaskItem( 3, "Text3"),
+        TaskItem( 4, "Text4"),
+        TaskItem( 5, "Text5"),
+    )
     val listState = rememberLazyListState()
 
     LazyColumn(state = listState) {
         items(items.size) { it ->
             Item(
                 items[it],
-                onClicked = { OnClickTaskItem(items[it].id) },
+                onClicked = { OnClickTaskItem(context, items[it].id) },
             )
             Divider()
         }
     }
 }
 
-fun OnClickTaskItem(id: Long){
-
+fun OnClickTaskItem(context: Context, id: Long) {
+    Toast.makeText(context, "Click $id", Toast.LENGTH_SHORT).show()
 }
 
 @Composable
@@ -50,7 +63,7 @@ private fun Item(
 
     Row(modifier = Modifier.clickable(onClick = onClicked)) {
 
-        Spacer(modifier = Modifier.width(8.dp))
+//        Spacer(modifier = Modifier.width(20.dp))
 
 //        Checkbox(
 //            checked = item.isDone,
@@ -65,6 +78,7 @@ private fun Item(
             modifier = Modifier
                 .weight(1F)
                 .align(Alignment.CenterVertically),
+            fontSize = 40.sp,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
